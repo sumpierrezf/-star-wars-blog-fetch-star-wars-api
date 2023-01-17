e9b96377b78dbc19e32fe50613871afe9c197e52
sumpierrezf <viqui09umpierrez@gmail.com>
2023-01-17 21:09:28 +0000

    Ahora se ven las dos Views

--- src/js/views/viewCharacters.jsx
+++ src/js/views/viewCharacters.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "../../styles/home.css";
 
 export const ViewCharacters = () => (
-	<div className="card mb-3" style="max-width: 540px;">
+	<div className="card mb-3" style={{maxWidth: "540px"}}>
   <div className="row g-0">
     <div className="col-md-4">
       <img src="..." className="img-fluid rounded-start" alt="..."/>
